4f8293fbfba9674db4512cf29d68e5345e3b3480
jscodez <jason20@umbc.edu>
2026-06-11 22:08:09 -0400

    Corrected and add printMap for debugging
    Refactor isIsomorphic function to use contains method for unordered maps and added printMap function for debugging.

--- leetcode/isomorhpic_strings.cpp
+++ leetcode/isomorhpic_strings.cpp
@@ -10,35 +10,40 @@ using namespace std;
 
 class Solution {
 public:
+
+    void printMap (unordered_map<char, char> &s_map) {
+        for (auto& pair : s_map) {
+
+            cout << pair.first << " : " << pair.second << endl;
+        }
+
+    }
     bool isIsomorphic(string s, string t) {
         unordered_map<char, char> s_map;
         unordered_map<char, char> t_map;
 
         for (int i = 0; i < s.length(); i++) {
-            char s_char = s[i];
-            char t_char = t[i];
 
-            if (s_map.count(s_char)) {
-                if (s_map[s_char] != t_char) {
-                    return false;
-
-                }
-            }
-            else {
-                s_map[s_char] = t_char;
-            }
-
-
-
-            if (t_map.count(s_char)) {
-                if (t_map[t_char] != s_char) {
+            if (s_map.contains(s[i])) {
+                if (t[i] != s_map[s[i]]) {
                     return false;
                 }
             }
-            else {
-                t_map[t_char] = s_char;
+            if (t_map.contains(t[i])) {
+                if (s[i] != t_map[t[i]]) {
+                    return false;
+                }
             }
+
+
+
+            s_map[s[i]] = t[i];
+            t_map[t[i]] = s[i];
+
+
         }
+
+        printMap(s_map);
 
 
 
@@ -50,8 +55,8 @@ public:
 
 
 int main() {
-    string s = "egg";
-    string t = "add";
+    string s = "badc";
+    string t = "baba";
 
     Solution ans;
     if (ans.isIsomorphic(s,t) == 1) {
